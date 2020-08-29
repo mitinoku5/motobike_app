fef9c8652b151ae76411767704d6fb2a5b699b67
RenHirama <rarmy0225@gmail.com>
2020-08-29 08:19:14 +0000

    static pages update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  #ページごとの完全なタイトル
+  def full_title(page_title = '')
+    base_title = "MotoBike App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end  
 end
